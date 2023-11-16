9c93bd53c1676b41e3d2e98d92cd88fb88544c5b
Katherine Paucar <gaby2091@outlook.es>
2023-11-16 21:17:21 +0100

    Instalar dependencias dev
    Instalar dependencias

--- 1-Bundling-Basico/webpack.config.js
+++ 1-Bundling-Basico/webpack.config.js
@@ -1,22 +1,46 @@
 import HtmlWebpackPlugin from "html-webpack-plugin"; // a partir de node 18
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "node:path";
+import url from "node:url";
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 export default {
-    entry: ["./src/index.js"],
+    context: path.resolve(__dirname, "src"),
+    entry: {
+        app: "./index.js"
+    },
+    output:{
+        filename: "[name].[chunkhash].js",
+        clean: true
+    },
     module: {
       rules: [ // LOADER 
         {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
         },
+        {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/, //Incluir en caso de instalar boostrap
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+        }
       ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-          template: "./src/index.html",
+          template: "./index.html",
           filename: "index.html",
           scriptLoading: "blocking",
-          hash: true,
         }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[chunkhash].css",
+            chunkFilename: "[id].css",
+          }),
       ],
   };
   
