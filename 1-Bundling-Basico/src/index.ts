a026903a64a11feee01f386350138ee2fcfed505
Katherine Paucar <gaby2091@outlook.es>
2023-11-17 18:07:28 +0100

    actualizar ficheros
    Actualizar estilos y html

--- 1-Bundling-Basico/src/index.ts
+++ 1-Bundling-Basico/src/index.ts
@@ -1,5 +1,9 @@
 import "./styles.scss";
 import img1  from "./images/image1.jpg";
+import img2  from "./images/image2.jpg";
 const img = document.createElement('img');
 img.src= img1;
 document.getElementById('containerImg').appendChild(img);
+let timer = setTimeout(() => {
+    img.src = img2;
+}, 4000);
